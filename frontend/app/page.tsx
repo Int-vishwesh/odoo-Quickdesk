73f5524fecc70d84391a1ac4eb65706b9bf0a8f1
Vishwesh Aryan <148064350+Int-vishwesh@users.noreply.github.com>
2025-08-02 11:31:03 +0530

    merged

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+    <main>
+      <header>
+        <h2 className="text-white">QuickDesk</h2>
+      </header>
+    </main>
     </>
   );
 }
